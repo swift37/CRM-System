881431b5c16d114c0163c4bbd1baabea4338b4b3
Swift <rafchik37@gmail.com>
2023-04-19 18:56:30 +0200

    MainViewModel, ViewModelLocator

---
+++ Librarian/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Swftx.Wpf.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librarian.ViewModels
+{
+    class MainViewModel : ViewModel
+    {
+        #region Tilte
+        private string? _Title = "Librarian";
+
+        /// <summary>
+        /// Window title
+        /// </summary>
+        public string? Title { get => _Title; set => Set(ref _Title, value); } 
+        #endregion
+    }       
+}

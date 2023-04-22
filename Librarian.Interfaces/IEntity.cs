30b6d3c9e4ab132f297136cb698d67401df37e5f
Swift <rafchik37@gmail.com>
2023-04-22 16:57:45 +0200

    Создание проекта с интерфейсами

---
+++ Librarian.Interfaces/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Librarian.Interfaces
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
---
+++ Librarian.Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace Librarian.Interfaces
+{
+    public interface IRepository<T> where  T : class, IEntity, new()
+    {
+        IQueryable<T>? Entities { get; }
+
+        T? Get(int id);
+
+        Task<T?>? GetAsync(int id, CancellationToken cancellation = default);
+
+        T? Add(T entity);
+
+        Task<T?>? AddAsync(T entity, CancellationToken cancellation = default);
+
+        void Update(T entity);
+
+        Task UpdateAsync(T entity, CancellationToken cancellation = default);
+
+        void Remove(int id);
+
+        Task RemoveAsync(int id, CancellationToken cancellation = default);
+    }
+}

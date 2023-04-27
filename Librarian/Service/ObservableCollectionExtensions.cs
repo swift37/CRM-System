90cdcc9c6a4528b02ff28d66459825f87e00533e
Swift <rafchik37@gmail.com>
2023-04-27 12:07:43 +0200

    ObservableCollectionExtensions

---
+++ Librarian/Service/ObservableCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace System.Collections.ObjectModel
+{
+    public static class ObservableCollectionExtensions
+    {
+        public static void Add<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+                collection.Add(item);
+        }
+    }
+}

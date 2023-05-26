8bec02e36db9aa8d4068bd6ed06b447a2d8be03c
Swift <swiftx37@gmail.com>
2023-05-26 18:27:36 +0200

    Display current user and dividing the window into 2 parts

--- Librarian/Views/Windows/MainWindow.xaml.cs
+++ Librarian/Views/Windows/MainWindow.xaml.cs
@@ -1,7 +1,17 @@
-﻿namespace Librarian
+﻿using System.Windows.Input;
+
+namespace Librarian
 {
     public partial class MainWindow
     {
         public MainWindow() => InitializeComponent();
+
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
     }
 }

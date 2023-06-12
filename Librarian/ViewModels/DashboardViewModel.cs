1e35e3c8b01d9cac9e17555c8e59d08ae0c20a74
Swift <swiftx37@gmail.com>
2023-06-12 20:38:09 +0200

    Отображение последних выполненных сделок на главной панели приложения

--- Librarian/ViewModels/DashboardViewModel.cs
+++ Librarian/ViewModels/DashboardViewModel.cs
@@ -1,14 +1,70 @@
-﻿using Swftx.Wpf.ViewModels;
+﻿using Librarian.DAL.Entities;
+using Librarian.Infrastructure.DebugServices;
+using Librarian.Interfaces;
+using Librarian.Services;
+using Microsoft.EntityFrameworkCore;
+using Swftx.Wpf.Commands;
+using Swftx.Wpf.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Librarian.ViewModels
 {
     public class DashboardViewModel : ViewModel
     {
+        private readonly IRepository<Transaction> _transactionsRepository;
 
+        #region Properties
+
+        #region LastTransactions
+        private ObservableCollection<Transaction>? _LastTransactions;
+
+        /// <summary>
+        /// Last Transactions
+        /// </summary>
+        public ObservableCollection<Transaction>? LastTransactions { get => _LastTransactions; set => Set(ref _LastTransactions, value); } 
+        #endregion
+
+        #endregion
+
+        #region Commands
+
+        #region LoadLastTransactionsCommand
+        private ICommand? _LoadLastTransactionsCommand;
+
+        /// <summary>
+        /// Load data command
+        /// </summary>
+        public ICommand? LoadLastTransactionsCommand => _LoadLastTransactionsCommand ??= new LambdaCommandAsync(OnLoadLastTransactionsCommandExecuted, CanLoadLastTransactionsCommandExecute);
+
+        private bool CanLoadLastTransactionsCommandExecute() => true;
+
+        private async Task OnLoadLastTransactionsCommandExecuted()
+        {
+            if (_transactionsRepository.Entities is null) return;
+
+            LastTransactions = (await _transactionsRepository.Entities.OrderByDescending(t => t.TransactionDate).Take(7).ToArrayAsync()).ToObservableCollection();
+        }
+        #endregion
+
+        #endregion
+
+        public DashboardViewModel() : this(new DebugTransactionsRepository())
+        {
+            if (!App.IsDesignMode)
+                throw new InvalidOperationException(nameof(App.IsDesignMode));
+
+            _ = OnLoadLastTransactionsCommandExecuted();
+        }
+
+        public DashboardViewModel(IRepository<Transaction> transactionsRepository) 
+        {
+            _transactionsRepository = transactionsRepository;
+        }
     }
 }
--- Librarian/Views/Design Views/OrderView.xaml.cs
+++ Librarian/Views/Design Views/OrderView.xaml.cs
@@ -19,15 +19,26 @@ namespace Librarian.Views
         public string Title { get => (string)GetValue(TitleProperty); set => SetValue(TitleProperty, value); }
         #endregion
 
-        #region DescProperty
-        public static readonly DependencyProperty DescProperty =
+        #region Desc_1Property
+        public static readonly DependencyProperty Desc_1Property =
             DependencyProperty.Register(
-                "Desc",
+                "Desc_1",
                 typeof(string),
                 typeof(OrderView),
                 new PropertyMetadata(default(string)));
 
-        public string Desc { get => (string)GetValue(DescProperty); set => SetValue(DescProperty, value); }
+        public string Desc_1 { get => (string)GetValue(Desc_1Property); set => SetValue(Desc_1Property, value); }
+        #endregion
+
+        #region Desc_2Property
+        public static readonly DependencyProperty Desc_2Property =
+            DependencyProperty.Register(
+                "Desc_2",
+                typeof(string),
+                typeof(OrderView),
+                new PropertyMetadata(default(string)));
+
+        public string Desc_2 { get => (string)GetValue(Desc_2Property); set => SetValue(Desc_2Property, value); }
         #endregion
 
         #region IconProperty

4072a4e4c99cb54cd1dd4175e800901b31164c13
Swift <swiftx37@gmail.com>
2023-07-03 17:16:09 +0200

    RuleBuilderExtensions

---
+++ Librarian/Tools/RuleBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Librarian.Tools
+{
+    public static class RuleBuilderExtensions
+    {
+        public static void Password<T>(this IRuleBuilder<T, string?> ruleBuilder, int minLenght = 8)
+        {
+            ruleBuilder
+                .MinimumLength(minLenght)
+                .WithMessage($"Minimum length wasn`t satisfied properly {minLenght}")
+                .Matches("[a-z]")
+                .WithMessage($"You need to have at least one lowercase letter")
+                .Matches("[A-Z]")
+                .WithMessage($"You need to have at least one uppercase letter")
+                .Matches("[0-9]")
+                .WithMessage($"You need to have at least one digit")
+                .Matches("[^a-zA-Z0-9]")
+                .WithMessage($"You need to have at least one special character");
+        }
+    }
+}

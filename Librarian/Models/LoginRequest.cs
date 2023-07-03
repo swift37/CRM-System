d0d917e2696df9469cfe551bb7169146eaac70ae
Swift <swiftx37@gmail.com>
2023-07-03 17:22:39 +0200

    LoginRequest and RegisterRequest models

---
+++ Librarian/Models/LoginRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Librarian.Models
+{
+    public class LoginRequest
+    {
+        public string? Login { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
---
+++ Librarian/Models/RegisterRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Librarian.Models
+{
+    public class RegisterRequest
+    {
+        public string? Login { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? PermissionLevel { get; set; }
+    }
+}
